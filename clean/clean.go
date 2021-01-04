04668bdede90e5547493a6de8d9ba70c1b689c0b
lutfuahmet <l6504167@std.yildiz.edu.tr>
2021-01-05 00:43:41 +0300

    update

--- clean/clean.go
+++ clean/clean.go
@@ -26,7 +26,7 @@ func New(config *auth.Config) *auth.Auth {
 	if config == nil {
 		config = &auth.Config{}
 	}
-	config.ViewPaths = append(config.ViewPaths, "github.com/qor/auth_themes/clean/views")
+	config.ViewPaths = append(config.ViewPaths, "github.com/lutfuahmet/auth_themes/clean/views")
 
 	if config.DB == nil {
 		fmt.Print("Please configure *gorm.DB for Auth theme clean")
@@ -36,7 +36,7 @@ func New(config *auth.Config) *auth.Auth {
 		yamlBackend := yaml.New()
 		I18n := i18n.New(yamlBackend)
 		for _, gopath := range append([]string{filepath.Join(utils.AppRoot, "vendor")}, utils.GOPATH()...) {
-			filePath := filepath.Join(gopath, "src", "github.com/qor/auth_themes/clean/locales/en-US.yml")
+			filePath := filepath.Join(gopath, "src", "github.com/lutfuahmet/auth_themes/clean/locales/en-US.yml")
 			if content, err := ioutil.ReadFile(filePath); err == nil {
 				translations, _ := yamlBackend.LoadYAMLContent(content)
 				for _, translation := range translations {
@@ -58,19 +58,6 @@ func New(config *auth.Config) *auth.Auth {
 	}
 
 	Auth := auth.New(config)
-
-	Auth.RegisterProvider(password.New(&password.Config{
-		Confirmable: true,
-		RegisterHandler: func(context *auth.Context) (*claims.Claims, error) {
-			context.Request.ParseForm()
-
-			if context.Request.Form.Get("confirm_password") != context.Request.Form.Get("password") {
-				return nil, ErrPasswordConfirmationNotMatch
-			}
-
-			return password.DefaultRegisterHandler(context)
-		},
-	}))
 
 	if Auth.Config.DB != nil {
 		// Migrate Auth Identity model
